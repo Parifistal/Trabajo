4e5740646fbe1cc05d78472c135a751f5919014b
Parifistal <159070012+Parifistal@users.noreply.github.com>
2024-03-01 08:26:16 -0500

    Menu de la calculadora

--- Principal.c
+++ Principal.c
@@ -3,3 +3,13 @@ int main (){
 int opcion, num1, num2;
 float resultado;
 system("clear");
+std::cout<<"Ingrese el primer numero entero"<<std::endl;
+std::cin>>num1;
+std::cout<<"Ingrese el segundo número entero"<<std::endl;
+std::cin>>num2;
+system("clear");
+
+std::cout<<":::::::::::MENU CALCULADORA:::::::::::::"<<std::endl;
+std::cout<<" "<<std::endl;
+std::cout<<"\t 1. Suma"<<std::endl;
+std::cout<<"\t 2. Resta \n\t 3.Producto \n\t 4.División \n\t 5.Modulo"<<std::endl;
